0a13f6883ffd479d7e9d5465d0a6610cf4baa5d1
Labib-ahammed <labibahammed03@gmail.com>
2024-10-23 18:35:35 +0600

    vercel

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,37 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors'
-import connectToDB from './db.js';
-import bookRoutes from './routes/books.route.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectToDB from "./db.js";
+import bookRoutes from "./routes/books.route.js";
+import helmet from "helmet";
 dotenv.config();
 
 connectToDB();
 const app = express();
-const allowedOrigins = ['http://localhost:5174', 'http://localhost:5173'];
 
-app.use(cors({
-  origin: ["https://mern-book-app-alpha.vercel.app"], // Replace with your actual domain
-  methods: ["POST", "GET", "DELETE"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://mern-book-app-alpha.vercel.app"], // Replace with your actual domain
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://vercel.live"],
+      },
+    },
+  })
+);
 
 app.use(express.json()); // Add middleware to parse JSON bodies
 
-app.use('/api', bookRoutes)
+app.use("/api", bookRoutes);
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+  console.log("Server is running on port 5000");
 });
