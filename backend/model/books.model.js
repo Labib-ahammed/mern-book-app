55ec782257145f37dd510339e900ee30739fe2d9
Shale ahammed jafor labib <labibahammed03@gmail.com>
2024-11-12 11:56:19 +0600

    Update books.model.js

--- backend/model/books.model.js
+++ backend/model/books.model.js
@@ -18,7 +18,7 @@ const bookSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-})
+},{ timestamps: true })
 
 const Book = mongoose.model("Book", bookSchema);
 export default Book;
